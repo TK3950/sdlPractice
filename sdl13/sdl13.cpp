edf086b13a6c62f09ffa39e7a1ccd0917f7e6d65
Timothy Kersten <kerstent4@gmail.com>
2017-03-30 00:51:23 -0400

    Modularizing methods

--- sdl13/sdl13.cpp
+++ sdl13/sdl13.cpp
@@ -6,50 +6,60 @@
 #undef main
 
 
+void UpdateScreen(SDL_Renderer* re, SDL_Surface* surf)
+{
+	SDL_BlitSurface(surf, NULL, surf, NULL);
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(re, surf);
+	SDL_RenderCopy(re, texture, NULL, NULL);
+	SDL_RenderPresent(re); // show
+	SDL_Delay(5);
+}
+
+
+void SetRenderer(SDL_Renderer* re)
+{
+	{
+		// calculations for objects to draw
+		SDL_SetRenderDrawColor(re, 255, 255, 255, 255); // object, r,g,b, alpha
+		SDL_RenderClear(re);
+		SDL_SetRenderDrawColor(re, 0, 0, 0, 255); // object, r,g,b, alpha
+		rhombus *rhombus1 = new rhombus(30, 30, 400, 200);
+
+		rhombus1->drawRhombus(re, *rhombus1);
+	}
+
+
+}
+
+
 int main()
 {
 	bool quit = false;
-	int i = 0;
-
-	// SDL Events
 	SDL_Event e;
-
-
 	SDL_Surface* m_surface = NULL; // create surface object, referenced by pointer
-	m_surface = SDL_LoadBMP("");
-	SDL_Renderer* renderer; // create renderer object, reference by pointer
+	SDL_Renderer* renderer = NULL; // create renderer object, reference by pointer
 	SDL_Init(SDL_INIT_EVERYTHING); // initialize all SDL layers
 	SDL_Window* screen = SDL_CreateWindow("SDL Demo", // create our window objecdt reference by pointer, with Title
 		SDL_WINDOWPOS_UNDEFINED, // x position
 		SDL_WINDOWPOS_UNDEFINED, // y position
 		640, 480, // width , height
 		SDL_WINDOW_OPENGL); // window flags
-	while(!quit)
+	m_surface = SDL_LoadBMP("");
+	renderer = SDL_CreateRenderer(screen, -1, 0); // make renderer-type object from screen, assign to renderer object
+
+	while (!quit)
 	{
-		renderer = SDL_CreateRenderer(screen, -1, 0); // make renderer-type object from screen, assign to renderer object
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // object, r,g,b, alpha
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // object, r,g,b, alpha
-		rhombus *rhombus1 = new rhombus(30, 30, 400, 200);
-		
-		rhombus1->drawRhombus(renderer, *rhombus1 );
-		SDL_BlitSurface(m_surface, NULL, m_surface, NULL);
-		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, m_surface);
-		SDL_RenderCopy(renderer, texture, NULL, NULL);
-		SDL_RenderPresent(renderer); // show
-		SDL_Delay(5);
-
-		while (SDL_PollEvent(&e) != 0)
-			if (e.type == SDL_KEYDOWN)
+		SDL_PollEvent(&e);
+		if (e.type == SDL_KEYDOWN)
+		{
+			if (e.key.keysym.scancode == SDL_SCANCODE_Q)
 			{
-				if (e.key.keysym.scancode == SDL_SCANCODE_Q)
-				{
-					quit = true;
-				}
+				quit = true;
 			}
-		
+		}
+		SetRenderer(renderer);
+		UpdateScreen(renderer, m_surface);
 	}
-
 	SDL_Delay(500);
 	SDL_FreeSurface(m_surface);
 	SDL_Quit();
