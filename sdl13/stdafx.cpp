6c5279af3cb19de057cbbfc0fcef2f7eea980ee0
Timothy <t.kersten.dev@gmail.com>
2017-04-05 16:13:55 -0400

    Changed to Bresenham ellipse, computes a millisecond or two faster at default size.

--- sdl13/stdafx.cpp
+++ sdl13/stdafx.cpp
@@ -4,6 +4,7 @@
 #pragma once
 #include "stdafx.h"
 #include <SDL\SDL.h>
+#define BRESENHAM 1
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
@@ -48,6 +49,8 @@ void shape::drawRectangle(SDL_Renderer *r, shape t) {
 void shape::drawEllipse(SDL_Renderer *r, shape t) {
 	double a = t.width / 2;
 	double b = t.height / 2;
+#ifndef BRESENHAM
+
 	SDL_RenderDrawPoint(r, t.posx + a, t.posy);
 	SDL_RenderDrawPoint(r, t.posx + a, t.posy + (b*2));
 	SDL_RenderDrawPoint(r, t.posx , t.posy + b);
@@ -76,8 +79,52 @@ void shape::drawEllipse(SDL_Renderer *r, shape t) {
 			SDL_RenderDrawPoint(r, t.posx - x + a, t.posy + y + b);
 		}
 	}
+#endif
+
+#ifdef BRESENHAM
+	int width = t.width/2;
+	int height = t.height/2;
+	int xc = t.posx+a;
+	int yc = t.posy+b;
+	{
+		int a2 = width * width;
+		int b2 = height * height;
+		int fa2 = 4 * a2, fb2 = 4 * b2;
+		int x, y, sigma;
+
+		/* first half */
+		for (x = 0, y = height, sigma = 2 * b2 + a2*(1 - 2 * height); b2*x <= a2*y; x++)
+		{
+			SDL_RenderDrawPoint(r, xc + x, yc + y);
+			SDL_RenderDrawPoint(r, xc - x, yc + y);
+			SDL_RenderDrawPoint(r, xc + x, yc - y);
+			SDL_RenderDrawPoint(r, xc - x, yc - y);
+			if (sigma >= 0)
+			{
+				sigma += fa2 * (1 - y);
+				y--;
+			}
+			sigma += b2 * ((4 * x) + 6);
+		}
+
+		/* second half */
+		for (x = width, y = 0, sigma = 2 * a2 + b2*(1 - 2 * width); a2*y <= b2*x; y++)
+		{
+			SDL_RenderDrawPoint(r, xc + x, yc + y);
+			SDL_RenderDrawPoint(r, xc - x, yc + y);
+			SDL_RenderDrawPoint(r, xc + x, yc - y);
+			SDL_RenderDrawPoint(r, xc - x, yc - y);
+			if (sigma >= 0)
+			{
+				sigma += fb2 * (1 - x);
+				x--;
+			}
+			sigma += a2 * ((4 * y) + 6);
+		}
+	}
+#endif
 	
-	
+	a += 1;
 
 }
 
