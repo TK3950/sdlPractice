c42b9bc7e661614789caedda6f5ebe23bdaa5eab
Timothy Kersten <kerstent4@gmail.com>
2017-05-09 22:05:24 -0400

    SDL_Rect now appears smoothly. Resizing temporarily disabled.

--- sdl13/menu.cpp
+++ sdl13/menu.cpp
@@ -1,14 +1,15 @@
 #include "menu.h"
 
+
+menu::menu(int x, int y, int w, int h) : posx(x), posy(y), width(w), height(h), active(false)
+{
+	box = new SDL_Rect{ posx, posy, width, height };
+}
 void menu::drawDropDown(SDL_Renderer *r)
 {
-
-	box.x = posx;
-	box.y = posy;
-	box.w = width;
-	box.h = height;
-	
-	SDL_RenderDrawRect(r, &box);
+	SDL_SetRenderDrawColor(r, 140, 140, 140, 0);
+	SDL_RenderFillRect(r, box);
+	SDL_RenderDrawRect(r, box);
 }
 
 menu::~menu()
--- sdl13/menu.h
+++ sdl13/menu.h
@@ -22,11 +22,11 @@
 class menu
 {
 	public:
-		menu(int x, int y, int w, int h) : posx(x), posy(y), width(w), height(h), active(false) {}
+		menu(int x, int y, int w, int h);
 		~menu();
 		void drawDropDown(SDL_Renderer *r);
 
-		SDL_Rect box;
+		SDL_Rect* box;
 	int posx;
 	int posy;
 	int width;
